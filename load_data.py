e4dd574f76d949389a60163b58ce424da9256172
royhsimbolon <roysimbolon005@gmail.com>
2025-06-02 14:44:33 +0700

    Revert "caching"
    This reverts commit d72c989bbcdee0d4f58d9c62e85d335229909649.

--- load_data.py
+++ load_data.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import gdown
 import os
-import streamlit as st
 
 files = {
     'job_postings_fact.csv': '1drExsbrK32VjG5Xh_CAmtZ7jtPs-S0fU',
@@ -9,13 +8,12 @@ files = {
     'skills_job_dim.csv': '1eq7osBABUfja8W3nO5S1JrQK50pYN4nK'
 }
 
-@st.cache_data(show_spinner=False)
 def download_and_load_csv():
     dataframes = {}
     for filename, file_id in files.items():
         url = f'https://drive.google.com/uc?id={file_id}'
         if not os.path.exists(filename):
-            gdown.download(url, filename, quiet=True)
+            gdown.download(url, filename, quiet=False)
         df = pd.read_csv(filename)
         dataframes[filename] = df
     return dataframes
