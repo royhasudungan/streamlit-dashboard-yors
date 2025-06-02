d6821e0ef1d8c9a2b065a3deb7c8c81f35915dc8
royhsimbolon <roysimbolon005@gmail.com>
2025-06-02 12:38:02 +0700

    test gdown buat file >100mb

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 import streamlit as st
 import pandas as pd
+import gdown
+import os
 
-st.title("Load CSV <100 mb from Google Drive 🚀")
+st.title("Load Large CSV (>100mb) from Google Drive with gdown 🚀")
 
-file_id = '1yqlyO577VtMn-xfJ2g_efxwaM0H8Y1dc'
-url = f'https://drive.google.com/uc?export=download&id={file_id}'
+file_id = '1drExsbrK32VjG5Xh_CAmtZ7jtPs-S0fU'
+url = f'https://drive.google.com/uc?id={file_id}'
+
+output = 'job_postings_fact.csv'
+
+# Download file cuma sekali, kalau belum ada
+if not os.path.exists(output):
+    with st.spinner("Downloading file..."):
+        gdown.download(url, output, quiet=False)
+else:
+    st.info("File already downloaded.")
 
 try:
-    df = pd.read_csv(url)
+    df = pd.read_csv(output)
     st.success("Data loaded successfully!")
     st.write(df.head())
 
