0d3b7675670ad000972045c7d9e678f0872b2998
royhsimbolon <roysimbolon005@gmail.com>
2025-06-10 11:27:26 +0700

    parquet part 2

--- load_data.py
+++ load_data.py
@@ -13,15 +13,21 @@ files = {
 def download_and_load_csv():
     dataframes = {}
     for filename, file_id in files.items():
-        url = f'https://drive.google.com/uc?id={file_id}'
-        if not os.path.exists(filename):
-            gdown.download(url, filename, quiet=True)
         parquet_file = filename.replace('.csv', '.parquet')
+
+        # Download CSV kalau belum ada
+        if not os.path.exists(filename):
+            url = f'https://drive.google.com/uc?id={file_id}'
+            gdown.download(url, filename, quiet=True)
+
+        # Load dari parquet kalau ada, kalau nggak load csv dan simpan parquet
         if os.path.exists(parquet_file):
             df = pd.read_parquet(parquet_file)
         else:
             df = pd.read_csv(filename, low_memory=False)
             df.to_parquet(parquet_file)
+
         dataframes[filename] = df
     return dataframes
+
 
